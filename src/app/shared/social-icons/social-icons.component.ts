48042e9f3301994ec7d8b91a291c7235be085b57
Mitchele <mitchele.centeno@lgusuite.com>
2024-08-02 09:53:32 +0800

    add icon config

--- src/app/shared/social-icons/social-icons.component.ts
+++ src/app/shared/social-icons/social-icons.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SOCIAL_MEDIA } from './social-icons.config';
 
 @Component({
   selector: 'app-social-icons',
@@ -6,4 +7,6 @@ import { Component } from '@angular/core';
   templateUrl: './social-icons.component.html',
   styleUrl: './social-icons.component.scss',
 })
-export class SocialIconsComponent {}
+export class SocialIconsComponent {
+  socialMedia = SOCIAL_MEDIA;
+}
---
+++ src/app/shared/social-icons/social-icons.config.ts
@@ -0,0 +1,27 @@
+export const SOCIAL_MEDIA = [
+  {
+    path: 'https://github.com/mitchelecenteno',
+    toolTip: 'GitHub',
+    icon: 'fa-brands fa-github',
+  },
+  {
+    path: 'https://www.linkedin.com/in/mitchele-centeno/',
+    toolTip: 'LinkedIn',
+    icon: 'fa-brands fa-linkedin',
+  },
+  {
+    path: 'https://codepen.io/mitchelecenteno',
+    toolTip: 'GitHub',
+    icon: 'fa-brands fa-codepen',
+  },
+  {
+    path: 'https://www.instagram.com/mitchelecenteno/',
+    toolTip: 'GitHub',
+    icon: 'fa-brands fa-instagram',
+  },
+  {
+    path: 'mailto:mitchele.centeno@gmail.com',
+    toolTip: 'GitHub',
+    icon: 'fa-solid fa-envelope',
+  },
+];
