431d7d4b7d2333e31fc1b659eb4ad1a2a63e1f24
Mitchele Centeno <mitchele.centeno@gmail.com>
2024-08-03 09:28:14 +0800

    fix soc icons padding

--- src/app/shared/send-message/send-message.config.ts
+++ src/app/shared/send-message/send-message.config.ts
@@ -1,6 +1,6 @@
 export const MESSAGE = [
   {
-    address: 'CVS Homes 1, Barangay San Isidro, Cainta Rizal Philippines.',
+    address: 'Cainta Rizal, Philippines.',
     mobileOne: '(+63) 921-425-3672',
     mobileTwo: '(+63) 976-106-5776',
     email: 'mitchele.centeno@gmail.com',
