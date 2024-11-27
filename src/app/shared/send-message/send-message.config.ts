66cdce6f0624e7ba9e3f2052bb9ccb825a4dc638
Mitchele <mitchele.centeno@lgusuite.com>
2024-11-27 09:56:59 +0800

    update contact num

--- src/app/shared/send-message/send-message.config.ts
+++ src/app/shared/send-message/send-message.config.ts
@@ -1,8 +1,8 @@
 export const MESSAGE = [
   {
     address: 'Cainta Rizal, Philippines.',
-    mobileOne: '(+63) 921-425-3672',
-    mobileTwo: '(+63) 976-106-5776',
+    mobileOne: '(+63) 976-528-4324',
+    // mobileTwo: '(+63) 976-106-5776',
     email: 'mitchele.centeno@gmail.com',
   },
 ];
