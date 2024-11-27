fc02fbdd307e27cc2c558099c0c0dab7e2a38a80
Mitchele <mitchele.centeno@lgusuite.com>
2024-11-27 09:58:10 +0800

    fix UI contact address

--- src/app/shared/send-message/send-message.config.ts
+++ src/app/shared/send-message/send-message.config.ts
@@ -1,6 +1,6 @@
 export const MESSAGE = [
   {
-    address: 'Cainta Rizal, Philippines.',
+    address: 'Cainta Rizal, Philippines',
     mobileOne: '(+63) 976-528-4324',
     // mobileTwo: '(+63) 976-106-5776',
     email: 'mitchele.centeno@gmail.com',
